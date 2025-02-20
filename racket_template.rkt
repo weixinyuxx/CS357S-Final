86063570ef7aff3fd902f164c540e6f308da6ca7
Weixin Yu <weixinyu@umich.edu>
2025-02-20 14:21:42 -0800

    Update racket template file to accommodate for different source registers

--- racket_template.rkt
+++ racket_template.rkt
@@ -11,24 +11,24 @@
 
 ; specification
 ; note: the translation for llvm ne should be (not (bveq reg1 reg2))
-(define (check impl %reg1 %reg2)
-  (define result (impl %reg1 %reg2))
+(define (check impl {{ left }} {{ right }})
+  (define result (impl {{ left }} {{ right }}))
   (assert (bveq result {{ expr }}))) 
 
 
 ; construct the grammar template
 ; note: there are no unary operations in llvm, so we do not model them here
-(define-grammar (binop_int %reg1 %reg2)
+(define-grammar (binop_int {{ left }} {{ right }})
   [expr
-    (choose %reg1 %reg2 (?? int?)
+    (choose {{ left }} {{ right }} (?? int?)
         ((bop) (expr) (expr)))]
   [bop
     (choose {{ rktop }})])
   
 
 ; expand the grammar template with depth
-(define (alter_op %reg1 %reg2)
-  (binop_int %reg1 %reg2 #:depth 2))
+(define (alter_op {{ left }} {{ right }})
+  (binop_int {{ left }} {{ right }} #:depth 2))
 
 ; synthesize
 (define-symbolic l r int?)
