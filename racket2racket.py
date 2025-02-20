01bfc3710264f6413f0ec0638757d51eee8319b6
Weixin Yu <weixinyu@umich.edu>
2025-02-20 14:21:00 -0800

    Fix bug: allow reg name to be any given accommodate for multiple lines expressions

--- racket2racket.py
+++ racket2racket.py
@@ -2,7 +2,7 @@ from jinja2 import Environment, FileSystemLoader
 import subprocess
 import re
 
-def render(expr, type, exclude, tmp_path):
+def render(expr, type, exclude, tmp_path, reg1, reg2):
     """Render the template racket file with the given parameters, writing to `tmp_path`."""
     try:
         assert type[0] == 'i'
@@ -17,7 +17,7 @@ def render(expr, type, exclude, tmp_path):
     ]
     rktop = list(set(rktop) - set(exclude))
     rktop = " ".join(rktop)
-    data = {"expr": expr, "type": type, "rktop": rktop}
+    data = {"expr": expr, "type": type, "rktop": rktop, "left": reg1, "right": reg2}
 
     # Set up the Jinja environment
     env = Environment(loader=FileSystemLoader("."))  # Load templates from current directory
@@ -30,26 +30,34 @@ def render(expr, type, exclude, tmp_path):
         f.write(racket_code)
     return 1
 
-def racket_run(tmp_path):
+def racket_run(tmp_path, reg1, reg2):
     """Run the temporary racket file in `tmp_path`, and extract the generated expression."""
     process = subprocess.Popen(
         ["racket", tmp_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
     )
-    pattern = re.compile(r"\(define\s+\(alter_op %reg1 %reg2\)\s*(\(.+\))")
+    result = ""
+    count = 0
     for line in process.stdout:
         # print(line)
-        match = pattern.search(line)
-        if match:
-            return match.group(1)
-    return None
+        if count == 0:
+            count += 1
+            continue
+        if (line == "#f\n"):
+            return None
+        result += line
+    temp = f"(define (alter_op {reg1} {reg2}) "
+    result = result[len(temp):]
+    result = result[:-2]
+    return result
 
-def r2r(expr, type, exclude, tmp_path="tmp.rkt"):
+def r2r(expr, type, exclude, reg1, reg2, tmp_path="tmp.rkt"):
     """Translate from racket to racket code."""
-    if render(expr, type, exclude, tmp_path) == None:
+    if render(expr, type, exclude, reg1, reg2,tmp_path) == None:
         return None
-    result = racket_run(tmp_path)
+    result = racket_run(tmp_path, reg1, reg2)
     if result == None:
         return None
     return result
 
-print(r2r("(bvadd %reg2 %reg2)", "i64", ["bvadd"]))
+print(r2r("(bvadd %reg2 %reg2)", "i64", ["bvadd"], "%reg1", "%reg2"))
+
