bff076d8b943ee1d477afa51409ed5e3cd64d598
Weixin Yu <weixinyu@umich.edu>
2025-02-24 19:04:58 -0800

    fix constant expressions: replace with alloca + store

--- rkt2llvm.py
+++ rkt2llvm.py
@@ -51,7 +51,8 @@ def rkt2llvm_helper(rkt_str, iterator, type, dest_reg):
     dest = dest_reg.alt_dest()
     if opcode == "bv":
         val = int(first_dest[2:], 16)
-        inst = [f"{dest} = add {type} {val}, 0"]
+        inst = [f"{dest} = alloca {type}",
+                f"store {type} {val}, {type}* {dest}"]
     else:
         inst = (
             first_inst
